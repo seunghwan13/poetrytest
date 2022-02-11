00c8504560bc65425eb101731b108519ffc76e6f
seunghwan13 <childwelfare23@gmail.com>
2022-02-11 11:50:56 +0900

    업데이트했습니다

--- poetrytest/asgi.py
+++ poetrytest/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poetrytest.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "poetrytest.settings")
 
 application = get_asgi_application()
