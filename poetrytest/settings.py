43eb072f6f9513912f7722b9ccf4c8493bfd4c2e
seunghwan13 <childwelfare23@gmail.com>
2022-02-12 01:10:28 +0900

    ci

--- poetrytest/settings.py
+++ poetrytest/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-from .my_settings import MY_DATABASES
+
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from typing import List
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "poetrytest",
 ]
 
 MIDDLEWARE = [
@@ -82,7 +81,16 @@ WSGI_APPLICATION = "poetrytest.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 pymysql.install_as_MySQLdb()
 
-DATABASES = MY_DATABASES
+MY_DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "sparta",
+        "USER": "root",
+        "PASSWORD": "14675821",
+        "HOST": "localhost",
+        "PORT": "3306",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
